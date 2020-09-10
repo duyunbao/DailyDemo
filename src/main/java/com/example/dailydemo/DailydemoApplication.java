5b028eb46e9c938d4e9cf61f1a068190ac47fc68
1114808306@qq.com <1114808306@qq.com>
2020-09-10 22:33:34 +0800

    初始化项目

---
+++ src/main/java/com/example/dailydemo/DailydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dailydemo/DailydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
